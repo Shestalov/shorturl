e37a1da161b650b2e70b22baeea815d621d38873
Alex <alexshestalov@gmail.com>
2022-11-02 15:09:30 +0200

    added telegram bot

--- main.py
+++ main.py
@@ -17,32 +17,31 @@ async def shorturl_post(request):
     result_text = await request.text()
     user_url = result_text.replace('user_url=', '')
     user_url = urllib.parse.unquote(user_url)
+    user_url_list = user_url.split('://')
     database = request.app["db"]
     collection = database['shorturl']
-    url_record = await collection.insert_one({'user_url': user_url})
+    # if url without http
+    if len(user_url_list) == 1:
+        url_record = await collection.insert_one({'user_url': user_url_list[0], 'prefix': 'http'})
+    else:
+        url_record = await collection.insert_one({'user_url': user_url_list[1], 'prefix': user_url_list[0]})
     return {'short_url': url_record.inserted_id}
 
 
 async def handler(request):
-    name_url = request.match_info.get('name')
-    try:
-        database = request.app['db']
-        collection = database['shorturl']
-        obj_url = await collection.find_one({'_id': ObjectId(name_url)})
-        select_url = obj_url['user_url']
-    except Exception as err:
-        return web.Response(text=str(err))
-
-    if 'http://' in select_url or 'https://' in select_url:
-        return web.HTTPFound(select_url)
-    else:
-        return web.HTTPFound('http://' + select_url)
+    name_url = request.match_info.get('link')
+    database = request.app['db']
+    collection = database['shorturl']
+    obj_url = await collection.find_one({'_id': ObjectId(name_url)})
+    url_without_prefix = obj_url['user_url']
+    prefix = obj_url.get('prefix', 'http')
+    return web.HTTPFound(prefix + '://' + url_without_prefix)
 
 
 db = asyncio.run(setup_db())
 app = web.Application()
 app.add_routes([web.get('/', shorturl_get),
-                web.get('/{name}', handler),
+                web.get('/{link}', handler),
                 web.post('/', shorturl_post)])
 aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader('templates'))
 app['db'] = db
